49835dda7b0cdb7995c12ebf6f9aaa292fb46f46
andreasp <dev@andreaspedersen.com>
2025-08-03 19:48:17 +0900

    add db/gain utils

--- python/pyIC/utils.py
+++ python/pyIC/utils.py
@@ -13,3 +13,19 @@ class IcUtils():
         exponent = int(math.floor(math.log10(abs(value)) / 3) * 3)
         mantissa = value / 10**exponent
         return f"{mantissa:.3f}e{exponent} {unit}".strip()
+
+    @staticmethod
+    def db2gain(db_val: float) -> float:
+        """
+        Converts a dB20 value to gain.
+        """
+        val = 10 ** (db_val / 20)
+        return val
+    
+    @staticmethod
+    def gain2db(gain_val: float) -> float:
+        """
+        Converts a gain value to dB20.
+        """
+        val = 20 * math.log10(gain_val)
+        return val
